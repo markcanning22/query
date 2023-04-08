ea3f7252236ba617b6522422c26d545acad918d4
Mark Canning <markcanning22@gmail.com>
2023-04-08 19:53:23 +0100

    Added ability for query to catch up with any missed events

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import axios, {AxiosResponse} from 'axios';
 
 const app = express();
 app.use(cors());
@@ -23,14 +24,15 @@ type Post = {
     comments: Comment[]
 }
 
+type Event = {
+    type: string,
+    data: any
+}
+
 const posts: {[index: string]: Post} = {};
 
-app.get('/posts', (req: Request, res: Response): void => {
-    res.send(posts);
-});
-
-app.post('/events', async (req: Request, res: Response): Promise<Response | undefined> => {
-    const {type, data} = req.body;
+const handleEvent = (event: Event) => {
+    const {type, data} = event;
 
     if (type === 'PostCreated') {
         const {id, title} = data;
@@ -65,18 +67,37 @@ app.post('/events', async (req: Request, res: Response): Promise<Response | unde
         }));
 
         if (comment === undefined) {
-            return res.send({message: 'Unable to find comment'}).status(404);
+            throw new Error('Unable to find comment');
         }
 
         comment.status = status;
         comment.content = content;
     }
+};
 
-    console.log(posts);
+app.get('/posts', (req: Request, res: Response): void => {
+    res.send(posts);
+});
+
+app.post('/events', (req: Request, res: Response): Response | undefined => {
+    try {
+        handleEvent(req.body);
+    } catch (error) {
+        return res.send({message: error}).status(500);
+    }
 
     res.send({});
 });
 
-app.listen(4002, (): void => {
+app.listen(4002, async (): Promise<void> => {
     console.log('Listening on 4002');
+
+    const res: AxiosResponse = await axios.get('http://localhost:4005/events');
+
+    let event: Event;
+    for (event of res.data) {
+        console.log('Processing event: ' + event.type);
+
+        handleEvent(event);
+    }
 });
