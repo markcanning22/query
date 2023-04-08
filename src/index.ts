83dcc6ba4e6f2448586dd09666ca29cc8f7bbae8
Mark Canning <markcanning22@gmail.com>
2023-04-08 18:49:07 +0100

    Added handling for CommentUpdated event and improved type safety

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,18 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-interface Comment
-{
-    id: string;
-    content: string;
+enum CommentStatus {
+    PENDING = 'pending'
 }
 
-interface Post
-{
+type Comment = {
+    id: string;
+    content: string;
+    status: CommentStatus
+    postId?: string;
+}
+
+type Post = {
     id: string;
     title: string;
     comments: Comment[]
@@ -25,20 +29,47 @@ app.get('/posts', (req: Request, res: Response): void => {
     res.send(posts);
 });
 
-app.post('/events', (req: Request, res: Response): void => {
+app.post('/events', async (req: Request, res: Response): Promise<Response | undefined> => {
     const {type, data} = req.body;
 
     if (type === 'PostCreated') {
         const {id, title} = data;
 
-        posts[id] = {id, title, comments: []};
+        posts[id] = {
+            id,
+            title,
+            comments: []
+        };
     }
 
     if (type === 'CommentCreated') {
-        const {id, content, postId} = data;
+        const {id, content, postId, status} = data;
 
         const post: Post = posts[postId];
-        post.comments.push({id, content});
+        const comment: Comment = {
+            id,
+            content,
+            status
+        };
+
+        post.comments.push(comment);
+    }
+
+    if (type === 'CommentUpdated') {
+        const {id, content, postId, status} = data;
+
+        const post = posts[postId];
+
+        const comment: Comment | undefined = post.comments.find((comment => {
+            return comment.id === id;
+        }));
+
+        if (comment === undefined) {
+            return res.send({message: 'Unable to find comment'}).status(404);
+        }
+
+        comment.status = status;
+        comment.content = content;
     }
 
     console.log(posts);
